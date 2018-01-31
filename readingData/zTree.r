323c007e99b5d382d2473f872e638c90462d6805
Jonathan <sifreed77@gmail.com>
2018-01-30 23:17:19 -0500

    Added zTrees reading implementation

---
+++ readingData/zTree.r
@@ -0,0 +1,20 @@
+#make sure the current working directory is set. E.g. setwd(yourProjectDataset)
+
+#store files read in a variable
+files <- lis.files(pattern = "[0-9]{6}_[0-9]{4}.xls$]", recursive=TRUE)
+trustGS <- zTreeTables(files)
+
+#save as Rdata file
+save(trustGS, zTreeTables, file="yourFileName.Rdata")
+
+#save in Stata-13 format
+saveGeneric<-with(trustGS, merge(globals, subjects))
+write.dta(saveGeneric, file="yourFileName.dta")
+
+#save in CSV
+write.csv(saveGeneric, file="yourFileName.csv")
+fn<-list.files(pattern="yourFileName\\.[^.]*")
+xtable(cbind(name=fn, size=file.size(fn)))
+
+#if two tables need to be merged
+with(trustGS, merge(globals, subjects))
