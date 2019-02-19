35bc2d935567414f4d96c601a3b04aebdfdca29c
Jonathan <sifreed77@gmail.com>
2019-02-18 23:03:32 -0500

    Update index.r

--- webdata/app1/index.r
+++ webdata/app1/index.r
@@ -1,5 +1,8 @@
 #installing package
 install.packages("data.table")
+#importing package
 library(data.table)
+#retrieving data
 mydata <- fread('http://www.stats.ox.ac.uk/pub/datasets/csb/97532.dat')
+#printing data top records
 head(mydata)
