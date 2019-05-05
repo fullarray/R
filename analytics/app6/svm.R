3288ecd01a1ac24ccdfdd4506d18ae8489b0676c
Jonathan <sifreed77@gmail.com>
2019-05-04 23:38:45 -0400

    Update svm.R

--- analytics/app6/svm.R
+++ analytics/app6/svm.R
@@ -5,7 +5,7 @@
 train <- read.csv("kddcup.data_10_percent_corrected")
 test <- read.csv("kddcup.testdata.labeled_10_percent")
 
-#Install and use SVM to train the model
+#Install and use SVM to train the models
 install.packages("kernlab")
 library(kernlab)
 classifier <- ksvm(labels ~ ., data = train, kernel = "vanilladot")
